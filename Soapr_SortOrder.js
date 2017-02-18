de15c924e8c3c54e7a996415136dbb87362c6da2
Soaprman <m.grieshober@gmail.com>
2017-02-18 12:46:20 -0500

    Add SortOrder script

---
+++ Soapr_SortOrder.js
@@ -0,0 +1,127 @@
+var Imported = Imported || {};
+Imported.Soapr_SortOrder = true;
+
+var Soapr = Soapr || {};
+Soapr.SortOrder = Soapr.SortOrder || {};
+
+//=============================================================================
+ /*:
+ * @plugindesc Allows you to specify a sort order in the database notes
+ * instead of reorganizing the database.
+ * @author Soaprman
+ *
+ * @help
+ * Help? What do you think this is, a professional script?
+ *
+ * ============================================================================
+ * Item/Skill/Weapon/Armor Note Tags
+ * ============================================================================
+ * <Sort Order: x>
+ * Change x to a number.
+ * When item/skill/weapon/armor lists are drawn, this value will be used
+ * instead of the item's database ID if specified.
+ *
+ * 
+ */
+//=============================================================================
+
+//=============================================================================
+// DataManager
+//=============================================================================
+
+Soapr.SortOrder.DataManager_isDatabaseLoaded = DataManager.isDatabaseLoaded;
+DataManager.isDatabaseLoaded = function () {
+    if (!Soapr.SortOrder.DataManager_isDatabaseLoaded.call(this)) return false;
+    if (!Soapr.SortOrder._databaseIsLoaded) {
+        this.processSortOrderNotetags($dataItems);
+        this.processSortOrderNotetags($dataSkills);
+        this.processSortOrderNotetags($dataWeapons);
+        this.processSortOrderNotetags($dataArmors);
+        Soapr.SortOrder._databaseIsLoaded = true;
+    }
+    return true;
+};
+
+DataManager.processSortOrderNotetags = function (group) {
+    for (var i = 1; i < group.length; i++) {
+        var item = group[i];
+        var noteData = item.note.split(/[\r\n]+/);
+
+        item.sortOrder = item.id;
+
+        for (var n = 0; n < noteData.length; n++) {
+            var line = noteData[n];
+            if (line.match(/<(?:Sort Order):[ ](\d+)>/i)) {
+                item.sortOrder = parseInt(RegExp.$1);
+            }
+        }
+    }
+};
+
+//=============================================================================
+// Window makeItemList functions
+//=============================================================================
+
+Soapr.SortOrder.sort = function (a, b) {
+    var aVal = a.sortOrder || a.id;
+    var bVal = b.sortOrder || b.id;
+    return aVal - bVal;
+};
+
+Soapr.SortOrder.Window_ItemList_makeItemList = Window_ItemList.prototype.makeItemList;
+Window_ItemList.prototype.makeItemList = function () {
+    Soapr.SortOrder.Window_ItemList_makeItemList.call(this);
+    this._data.sort(Soapr.SortOrder.sort);
+};
+
+Soapr.SortOrder.Window_SkillList_makeItemList = Window_SkillList.prototype.makeItemList;
+Window_SkillList.prototype.makeItemList = function () {
+    Soapr.SortOrder.Window_SkillList_makeItemList.call(this);
+    this._data.sort(Soapr.SortOrder.sort);
+};
+
+Soapr.SortOrder.Window_ShopBuy_makeItemList = Window_ShopBuy.prototype.makeItemList;
+Window_ShopBuy.prototype.makeItemList = function () {
+    Soapr.SortOrder.Window_ShopBuy_makeItemList.call(this);
+    this._data.sort(Soapr.SortOrder.sort);
+};
+
+if (Window_SkillLearn) {
+    Soapr.SortOrder.Window_SkillLearn_makeItemList = Window_SkillLearn.prototype.makeItemList;
+    Window_SkillLearn.prototype.makeItemList = function () {
+        Soapr.SortOrder.Window_SkillLearn_makeItemList.call(this);
+        this._data.sort(Soapr.SortOrder.sort);
+    };
+}
+
+if (Window_SkillLearnClass) {
+    Soapr.SortOrder.Window_SkillLearnClass_makeItemList = Window_SkillLearnClass.prototype.makeItemList;
+    Window_SkillLearnClass.prototype.makeItemList = function () {
+        Soapr.SortOrder.Window_SkillLearnClass_makeItemList.call(this);
+        this._data.sort(Soapr.SortOrder.sort);
+    };
+}
+
+if (Window_SnatchItem) {
+    Soapr.SortOrder.Window_SnatchItem_makeItemList = Window_SnatchItem.prototype.makeItemList;
+    Window_SnatchItem.prototype.makeItemList = function () {
+        Soapr.SortOrder.Window_SnatchItem_makeItemList.call(this);
+        this._data.sort(Soapr.SortOrder.sort);
+    };
+}
+
+if (Window_VictoryDrop) {
+    Soapr.SortOrder.Window_VictoryDrop_makeItemList = Window_VictoryDrop.prototype.makeItemList;
+    Window_VictoryDrop.prototype.makeItemList = function () {
+        Soapr.SortOrder.Window_VictoryDrop_makeItemList.call(this);
+        this._data.sort(Soapr.SortOrder.sort);
+    };
+}
+
+if (Window_VictorySkills) {
+    Soapr.SortOrder.Window_VictorySkills_makeItemList = Window_VictorySkills.prototype.makeItemList;
+    Window_VictorySkills.prototype.makeItemList = function () {
+        Soapr.SortOrder.Window_VictorySkills_makeItemList.call(this);
+        this._data.sort(Soapr.SortOrder.sort);
+    };
+}
